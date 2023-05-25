c21986f44c4a20fb327770d1c33a476b41cab8b5
Michal Devecka <michal.devecka@gmail.com>
2023-05-25 20:23:26 +0200

    update

--- server/server.js
+++ server/server.js
@@ -1,23 +1,22 @@
 // This file is to run a server in localhost:3000
 // Code to handle annotations is in annotationHandler.js
 
-const express = import('express');
-const bodyParser = import('body-parser');
-const open = import('open');
-const annotationHandler = import('./serverHandlers');
+const express = import("express");
+const bodyParser = import("body-parser");
+const open = import("open");
+const annotationHandler = import("./serverHandlers");
 const port = process.env.PORT || 5000;
-const cors = import("cors")
+const cors = import("cors");
 
 const app = express();
 
 app.use(bodyParser.text());
 // app.use('/client', express.static('client')); // For statically serving 'client' folder at '/'
-app.use(cors)
-app.use ('/', annotationHandler(app));
+app.use(cors);
+app.use("/", annotationHandler(app));
 
-
-app.get('/', (req, res) => {
-    res.send('Test');
-   });
+app.get("/", (req, res) => {
+  res.send("Test");
+});
 // Run server
 app.listen(port, () => console.log(`Listening on port ${port}`));
