94886b64ccbbd7120582a3ef1147e7f9d49c2590
Michal Devecka <michal.devecka@gmail.com>
2023-05-25 20:28:38 +0200

    storing pdfs

--- server/server.js
+++ server/server.js
@@ -1,22 +1,48 @@
 // This file is to run a server in localhost:3000
 // Code to handle annotations is in annotationHandler.js
 
-const express = import("express");
-const bodyParser = import("body-parser");
-const open = import("open");
-const annotationHandler = import("./serverHandlers");
+var corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+};
+
+const express = require("express");
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
-const cors = import("cors");
+const cors = require("cors");
+const SQLite3 = require("sqlite3").verbose();
+const TABLE = "annotations";
+const multer = require("multer");
+const { type } = require("os");
 
 const app = express();
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "../server/uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
 
-app.use(bodyParser.text());
-// app.use('/client', express.static('client')); // For statically serving 'client' folder at '/'
-app.use(cors);
-app.use("/", annotationHandler(app));
+const upload = multer({ storage });
+
+app.use("../../frontend", express.static("client")); // For statically serving 'client' folder at '/'
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Test");
 });
 // Run server
+
+app.post(
+  "/post",
+  cors(corsOptions),
+  upload.single("my blob"),
+  async (req, res) => {
+    console.log(req.file);
+    res.send("Test");
+  }
+);
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
